aaf74b0eacdafcff49d763d57196bd5463fe6692
231801061-Iraiyanbu <231801061@rajalakshmi.edu.in>
2026-01-19 09:21:46 +0530

    Add files via upload

---
+++ lab_3.py
@@ -0,0 +1,34 @@
+import pyotp, qrcode
+# Generate per-user secret 
+secret =pyotp.random_base32() 
+totp =pyotp.TOTP(secret)
+
+# Provisioning URI (scan with authenticator app)
+uri = totp.provisioning_uri(name="student@example.com",
+issuer_name="AD23631-MFA-Lab")
+qrcode.make(uri).save("totp-qr.png")
+print("Secret:",secret)
+import qrcode
+
+qr = qrcode.QRCode()
+qr.add_data(uri)
+qr.make()
+
+qr.print_ascii(invert=True)
+
+
+import time
+MAX_ATTEMPTS = 3
+attempts = 0
+while attempts < MAX_ATTEMPTS:
+    code = input("Enter the 6-digit code: ")
+
+    if totp.verify(code, valid_window=1):  # allows +/-30s drift
+        print("Authentication Successful")
+        break
+
+    attempts += 1
+    print("Invalid code. Attempts left:", MAX_ATTEMPTS - attempts)
+
+else:
+    print("Too many failures — account locked, alert security team")
